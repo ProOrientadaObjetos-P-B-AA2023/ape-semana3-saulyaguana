d9e6de1f8c82be4884301bf30c6baca3e77528ea
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-04-26 17:32:49 -0500

    V2

--- problema1/src/Problema1.java
+++ problema1/src/Problema1.java
@@ -1,6 +1,6 @@
 public class Problema1 {
     public static void main(String[] args) {
-        Terreno terreno1 = new Terreno(10, 2, 2);
+        Terreno terreno1 = new Terreno(13.23, 2.11, 2.4);
         terreno1.calcularAreaTerreno();
         terreno1.calcularCostoTerreno();
         System.out.println(terreno1);
